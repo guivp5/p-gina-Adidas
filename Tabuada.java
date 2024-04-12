eba594a47f162893bfa5acd53bbfc20a788cfb6b
Guilherme Vieira <elguilherme@gmail.com>
2024-04-12 19:55:21 -0300

    javinha

---
+++ Tabuada.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Tabuada {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int numero;
+        int i;
+
+        System.out.println("Escolha um numero:");
+        numero = scanner.nextInt();
+
+        for (i = 1; i <= 10; i++) {
+            System.out.println(numero + " x " + i + " = " + numero * i);
+        }
+        
+        scanner.close();
+    }
+
+}
