ca69239043cc63ed481bcc13345e401f3e24ec8a
Guilherme Vieira <elguilherme@gmail.com>
2024-04-12 21:11:30 -0300

    atualizando a pagina

--- Tabuada.java
+++ Tabuada.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class Tabuada {
     public static void main(String[] args) {
+        
         Scanner scanner = new Scanner(System.in);
         int numero;
         int i;
